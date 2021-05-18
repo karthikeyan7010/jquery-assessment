6306b63a19cf14562f784d8d19ddeba37f89a989
karthikeyan7010 <81284723+karthikeyan7010@users.noreply.github.com>
2021-05-18 11:08:11 +0530

    Add files via upload

---
+++ orderform/orderform.js
@@ -0,0 +1,142 @@
+$(document).ready(function() {
+    var count = 1;
+    var rows = [1];
+    var displayTable = false;
+    var table = $('#table1');
+    var tableBody = $("#table-body")
+    var tableBody = $('#table-body');
+
+    // Add Button----------------------------------------------->>>>>
+    $("#add").click(function() {
+        count = count + 1;
+        rows.push(count);
+        var head = $('#table-head');
+        var div = "<div id='order-row-" + count + "' class='row'>";
+        var row = $('#fields').html();
+        row = row.replace(/1/g, count);
+        row = row + '<button type="button" class="del-btn" value="' + count + '">x</button>';
+        div = div + row;
+        div = div + '</div>'
+        head.append(div);
+
+    })
+    // Delete Button----------------------------------------->>>>>
+
+    $(document).on('click', '.del-btn', function(event) {
+        let id = $(event.currentTarget).val();
+        var removeDivID = "#order-row-" + id;
+        $(removeDivID).remove();
+    });
+
+    // Reset Button --------------------------------------->>>>>>
+    $('#clear').click(function() {
+        for (let i = 0; i < rows.length; i++) {
+            $("#order-row-" + rows[i]).remove();
+        }
+        rows = [1];
+        table.css('display', 'none');
+        tableBody.html('');
+        $('#odd-id1').val('');
+        $('#vow-id1').val('');
+        $('#sor-id1').val('');
+        $("#lar-id1").val('');
+    })
+    // Submit Button-------------------------->>>>>
+    $("#submit").click(function() {
+
+        //Adding More Then Five Rows----------->>>>>
+
+        if (rows.length < 5) {
+            alert("Add more then five colum");
+            return false
+        }
+
+        var odd = [];
+        var vow = [];
+        var sor = [];
+        var lar = [];
+        for (let i = 1, j = 0; i <= rows.length; i++, j++) {
+            odd[j] = Number($('#odd-id' + i).val());
+            vow[j] = $('#vow-id' + i).val();
+            sor[j] = Number($('#sor-id' + i).val());
+            lar[j] = Number($("#lar-id" + i).val());
+
+            //Requried Fields--------------------------------------------------->>>>>
+
+            if (odd[j] == "" || vow[j] == "" || sor[j] == "" || lar[j] == "") {
+                alert("All input fields are requried")
+                return false
+            }
+        }
+
+        //Sorting the array---------------------------------------------------------------->>>>>>>>   
+
+        function sorting(sor) {
+            sor.sort();
+            console.log(sor);
+            $("#sor").text(sor)
+        }
+        sorting(sor);
+
+
+        //Finding minimum And Maxmium------------------------------------------------------------>>>>>>
+
+        function large(lar) {
+
+            minValue = Math.min(...lar);
+            maxValue = Math.max(...lar);
+            console.log("min==" + minValue);
+            console.log("max==" + maxValue);
+            $("#lar").text("[" + "min==" + minValue + "]" + "[" + "max==" + maxValue + "]")
+        }
+        large(lar);
+
+        // ODD And Even------------------------------------------------------------------------------->>>>
+
+        function oddeven(odd) {
+            var arr = [];
+            var arr1 = [];
+            for (let i = 0; i < odd.length; i++) {
+                if (odd[i] % 2 === 0) {
+                    console.log(odd[i] + " is even");
+                    arr.push(odd[i]);
+                } else {
+                    console.log(odd[i] + " is odd");
+                    arr1.push(odd[i]);
+                };
+            }
+
+
+            console.log(arr + "even");
+            console.log(arr1 + "odd");
+            $("#even").text("[" + arr + "even" + "]" + "[" + arr1 + "odd" + "]")
+        }
+        oddeven(odd)
+
+        //Find vowels in given text ------------------------------------------------------------------------>>>>
+
+        function countVowels(vow) {
+            var arr1 = [];
+            var sent = ['a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U'];
+            for (let i = 0; i < vow.length; i++) {
+                let counts = 0;
+                var name = vow[i];
+                for (let j = 0; j < name.length; j++) {
+                    if (sent.includes(name[j])) {
+                        counts++;
+                    }
+                }
+
+                console.log(name + "contains " + counts + "vowels" + "<br>");
+                arr1.push(name + "contains " + counts + "vowels" + "<br>");
+                $("#vow").text(arr1)
+
+            }
+        }
+        countVowels(vow);
+
+        // ---------------------------------------------------------------------------------------->>>>>>
+
+
+    })
+})
